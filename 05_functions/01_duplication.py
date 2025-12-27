0a92ac8a7fa5f51f0824bd994ff7c811323997e1
PayalKumari10 <kumaripayal7488@gmail.com>
2025-12-27 14:18:07 +0530

    Add initial implementations for order printing and sales report generation functions

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,9 @@
+def print_order(name, chai_type):
+    print(f"{name} ordered {chai_type} chai")
+
+
+print_order("Payal", "masala")
+print_order("Ankit", "ginger")
+print_order("Sonal", "cardamom")
+
+
