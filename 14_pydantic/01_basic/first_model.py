c7f10eb97ac3df45fa9c3ad894a2c4f93e799e3f
PayalKumari10 <kumaripayal7488@gmail.com>
2026-01-08 15:24:37 +0530

    Add Pydantic models for User and Product with example instances

--- 14_pydantic/01_basic/first_model.py
+++ 14_pydantic/01_basic/first_model.py
@@ -1,1 +1,16 @@
-print("Start of pydantic journey")
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name: str
+    is_active: bool
+
+input_data = {
+    "id": 101,
+    "name": "Chaicode",
+    "is_active": True
+}
+
+user = User(**input_data)
+print(user)
+
