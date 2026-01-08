6509bb222050178792dbab724d92b57a080c91e0
PayalKumari10 <kumaripayal7488@gmail.com>
2026-01-08 17:35:35 +0530

    Add Pydantic models for Booking, Product, Employee, User, and Cart with computed properties and validation

---
+++ 14_pydantic/01_basic/field_example.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import List, Dict, Optional
+
+class Cart(BaseModel):
+    user_id: int
+    items: List[str]
+    quantities: Dict[str, int]
+
+class BlogPost(BaseModel):
+    title: str
+    content: Optional[str] = None
+
+cart_data = {
+    "user_id": 123,
+    "items": ["Laptop","Mouse","Keyboard"],
+    "quantities": {"laptop": 1, "mouse":2, "keyboard": 3}
+}   
+
+cart = Cart(**cart_data)
